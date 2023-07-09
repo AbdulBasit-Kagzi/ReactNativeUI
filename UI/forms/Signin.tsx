c411e2a5353a7ab512b64399042333d3a22ecbb8
abdulbasitkagzi <abdulbasit.kagzi@propelius.tech>
2023-07-09 15:05:46 +0530

    added signp functionality with firbase authentication

--- UI/forms/Signin.tsx
+++ UI/forms/Signin.tsx
@@ -6,13 +6,39 @@ import {
   SafeAreaView,
   Button,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import auth from '@react-native-firebase/auth';
 
 interface SiginProps {
   navigation: any;
 }
 
 export default function Signin({navigation}: SiginProps) {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const createUser = () => {
+    console.log('check', email, password);
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    console.log('data', email, password);
+  }, [email, password]);
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -21,6 +47,7 @@ export default function Signin({navigation}: SiginProps) {
             style={styles.textField}
             placeholder="E-mail"
             placeholderTextColor={'#0A0A0A'}
+            onChangeText={text => setEmail(text)}
           />
         </View>
         <View style={styles.elevation}>
@@ -28,6 +55,7 @@ export default function Signin({navigation}: SiginProps) {
             style={styles.textField}
             placeholder="Password"
             placeholderTextColor={'#0A0A0A'}
+            onChangeText={text => setPassword(text)}
           />
         </View>
       </View>
@@ -37,13 +65,11 @@ export default function Signin({navigation}: SiginProps) {
         </Text>
       </View>
       <View>
-        <Button
-          title="Sign-in"
-          color="#ED6C50"
-          onPress={() => navigation.navigate('Createprofile')}
-        />
+        <Button title="Sign-in" color="#ED6C50" onPress={() => createUser()} />
       </View>
-     
+      <View style={{alignItems: 'center', marginTop: 48}}>
+        <Button title="I need to sign-up!" color="#0A0A0A" />
+      </View>
     </SafeAreaView>
   );
 }
--- UI/pages/Login.tsx
+++ UI/pages/Login.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Signin from '../forms/Signin';
 
 export default function Login({navigation}: any) {
